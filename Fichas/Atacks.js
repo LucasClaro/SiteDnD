06066ec28f6a8c5ed86561e7a2035450578c7543
Lucas Claro <lucasbrclaro@gmail.com>
2019-05-22 23:56:30 -0300

    22/05

--- Fichas/Atacks.js
+++ Fichas/Atacks.js
@@ -19,12 +19,20 @@ function AtualizarLista(){
     }
 
     for (let i = 0; i < Ataques.length; i++) {
-        const cloneItem = document.querySelector("#itemBase").cloneNode(false);
+        const cloneItem = document.querySelector("#itemBase").cloneNode(true);
         const parent = document.querySelector("#ataques");
         parent.appendChild(cloneItem);
         cloneItem.classList.remove("oculto");
         cloneItem.id = 'item-'+ i;
         cloneItem.classList.add("current-item");
-        cloneItem.textContent = Ataques[i];
+        cloneItem.querySelector("span").textContent = Ataques[i];
+        cloneItem.querySelector("button").id = 'btn-' + i;
+        cloneItem.querySelector("button").addEventListener("click", RemoverAttack);
     }
+}
+
+function RemoverAttack(e){    
+    const identificador = parseInt(e.target.id.substring(4));
+    Ataques.splice(identificador, 1);
+    AtualizarLista();
 }
