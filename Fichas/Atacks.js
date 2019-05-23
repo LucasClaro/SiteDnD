801ea61113963df672a8983e91c802c20e4c4275
Lucas Claro <lucasbrclaro@gmail.com>
2019-05-23 02:01:25 -0300

    23/05

--- Fichas/Atacks.js
+++ Fichas/Atacks.js
@@ -1,38 +1,86 @@
 var Ataques = [];
+var Magias = [];
+var MagiasTxt = [];
 
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#addAttack").addEventListener("click", function(){
-        const descricao = document.querySelector("#texto").value;
+        let descricao = "";
+
+        descricao += document.querySelector("#AttackNome").value;
+        descricao += " ( " + document.querySelector("#AttackBonus").value + " ) ";
+        descricao += document.querySelector("#AttackDano").value + " - ";
+        descricao += document.querySelector("#AttackTipo").value;
 
         Ataques.push(descricao);
-        AtualizarLista();
+        AtualizarListaAtaques();
     })
+
+    document.querySelector("#addMagia").addEventListener("click", function(){
+        const nome = document.querySelector("#textoMagia").value;
+        const descricao = document.querySelector("#txtMagia").value;
+
+        Magias.push(nome);
+        MagiasTxt.push(descricao);
+        AtualizarListaMagias();
+    })
+
 });
 
-function AtualizarLista(){
+function AtualizarListaAtaques(){
     var elements = document.querySelectorAll("li.item");
 
     for (let i = 0; i < elements.length; i++) {
-        if (elements[i].id != "itemBase") {
+        if (elements[i].id != "AtckBase") {
             elements[i].remove();
         }
     }
 
     for (let i = 0; i < Ataques.length; i++) {
-        const cloneItem = document.querySelector("#itemBase").cloneNode(true);
+        const cloneItem = document.querySelector("#AtckBase").cloneNode(true);
         const parent = document.querySelector("#ataques");
         parent.appendChild(cloneItem);
         cloneItem.classList.remove("oculto");
-        cloneItem.id = 'item-'+ i;
+        cloneItem.id = 'attack-'+ i;
         cloneItem.classList.add("current-item");
         cloneItem.querySelector("span").textContent = Ataques[i];
-        cloneItem.querySelector("button").id = 'btn-' + i;
+        cloneItem.querySelector("button").id = 'btnA-' + i;
         cloneItem.querySelector("button").addEventListener("click", RemoverAttack);
     }
 }
 
 function RemoverAttack(e){    
-    const identificador = parseInt(e.target.id.substring(4));
+    const identificador = parseInt(e.target.id.substring(5));
     Ataques.splice(identificador, 1);
-    AtualizarLista();
+    AtualizarListaAtaques();
+}
+
+function AtualizarListaMagias(){
+    var elements = document.querySelectorAll("div.item");
+
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].id != "MagiaBase") {
+            elements[i].remove();
+        }
+    }
+
+    for (let i = 0; i < Magias.length; i++){
+        const cloneItem = document.querySelector("#MagiaBase").cloneNode(true);
+        const parent = document.querySelector("#accordionExample");
+        parent.appendChild(cloneItem);
+        cloneItem.classList.remove("oculto");
+        cloneItem.id = 'magia-' + 1;
+        cloneItem.querySelector("#btnNomeMagia").textContent = Magias[i];
+        cloneItem.querySelector("#btnRemoverMagia").addEventListener("click", RemoverMagia);
+        cloneItem.querySelector("#divDescricaoMagia").textContent = MagiasTxt[i];
+        cloneItem.querySelector("#collapseOne").id = "collapse" + i;
+        cloneItem.querySelector("#btnNomeMagia").dataset.target = "#collapse" + i;
+        cloneItem.querySelector("#btnRemoverMagia").id = "btnM-" + i;
+    }
+}
+
+function RemoverMagia(e){    
+    const identificador = parseInt(e.target.id.substring(5));
+    Magias.splice(identificador, 1);
+    MagiasTxt.splice(identificador, 1);
+    AtualizarListaMagias();
 }
