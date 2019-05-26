76f56eeab791f68d45c5f3eaaff0c54e33d89fb0
Lucas Claro <lucasbrclaro@gmail.com>
2019-05-25 21:13:38 -0300

    25/05

--- Aparencia.js
+++ Aparencia.js
@@ -12,10 +12,29 @@ function readURL(input) {
     }    
 }
 
+function carregarURl(input) {
+    var ext = input.files[0]['name'].substring(input.files[0]['name'].lastIndexOf('.') + 1).toLowerCase();
+    if (input.files && input.files[0] && (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#imgSimbolo').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        $('#imgSimbolo').attr('src', '/assets/no_preview.png');
+    }    
+}
+
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("#imgAparencia").addEventListener("load", mudarTxtArea);
+    document.querySelector("#imgSimbolo").addEventListener("load", mudarTxtAreaKai);
 })
 
 function mudarTxtArea(){
-    document.querySelector("#historia").style.height = Math.floor(document.querySelector("#ImgBorda").offsetHeight) + "px";
+    document.querySelector("#historia").style.height = Math.floor(document.querySelector("#BordaAparencia").offsetHeight) + "px";
+}
+
+function mudarTxtAreaKai(){
+    document.querySelector("#aliados").style.height = (Math.floor(document.querySelector("#BordaSimbolo").offsetHeight) + 35) + "px";
 }
