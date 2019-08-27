a7dff5676695e8c977491392de983d2a2c67b404
Lucas Claro <lucasbrclaro@gmail.com>
2019-08-27 14:09:10 -0300

    27/08

--- SaveLoad.js
+++ SaveLoad.js
@@ -1,13 +1,15 @@
+var Personagem = new Object();
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#Salvar").addEventListener("click", salvar);
 })
 
 function salvar() {
-    var xmltext = criarTexto();
+    var xmltext = montarObj();
     var pom = document.createElement('a');
 
 
-    var filename = pegarNome() + ".xml";
+    var filename = pegarNome() + ".json";
     var pom = document.createElement('a');
     var bb = new Blob([xmltext], { type: 'text/plain' });
 
@@ -21,21 +23,51 @@ function salvar() {
     pom.click();
 }
 
-function criarTexto(){
-    let inicio = "<Personagem>\n";
+function montarObj(){
+    Personagem.Nome = pegarNome();
+    Personagem.Raca = document.querySelector("#Raca").value;
+    Personagem.Classe = document.querySelector("#Classe").value;
+    Personagem.Tendencia = document.querySelector("#Tendencia").value;
+
+    Personagem.xpAtual = document.querySelector("#xpAt").value;
+    Personagem.xpMax = document.querySelector("#xpMax").value;
+
+    Personagem.For = document.querySelector("#ForHab").value;
+    Personagem.Des = document.querySelector("#DesHab").value;
+    Personagem.Con = document.querySelector("#ConHab").value;
+    Personagem.Int = document.querySelector("#IntHab").value;
+    Personagem.Sab = document.querySelector("#SabHab").value;
+    Personagem.Car = document.querySelector("#CarHab").value;
+
+    Personagem.Ca = document.querySelector("#Ca").value;
+    Personagem.Bonus = document.querySelector("#bonus").value;
+    Personagem.Desloc = document.querySelector("#Desloc").value;
+    Personagem.Inspiracao = document.querySelector("#Inspiracao").value;
+    Personagem.Iniciativa = document.querySelector("#Iniciativa").value;
+    Personagem.PercepPassiva = document.querySelector("#PerPass").value;
+
     
-    let nome = "<Nome>" + pegarNome() + "</Nome>\n";
-    let raca = "<Raca>" + document.querySelector("#Raca").value + "</Raca>\n";
-    let classe = "<Classe>" + document.querySelector("#Classe").value + "</Classe>\n";
 
-    let tendencia = "<Tendencia>" + document.querySelector("#Tendencia").value + "</Tendencia>\n"
+    
+    return JSON.stringify(Personagem);;
 
-    let xp = "<Xp>\n<Atual>" + document.querySelector("#xpAt").value + "</Atual>\n<Max>" + document.querySelector("#xpMax").value + "</Max>\n</Xp>\n";
-
-    let fim = "</Personagem>";
-
-    return inicio + nome + raca + classe + tendencia + xp + fim;
 }
+
+// function criarTexto(){
+//     let inicio = "<Personagem>\n";
+    
+//     let nome = "<Nome>" + pegarNome() + "</Nome>\n";
+//     let raca = "<Raca>" + document.querySelector("#Raca").value + "</Raca>\n";
+//     let classe = "<Classe>" + document.querySelector("#Classe").value + "</Classe>\n";
+
+//     let tendencia = "<Tendencia>" + document.querySelector("#Tendencia").value + "</Tendencia>\n"
+
+//     let xp = "<Xp>\n<Atual>" + document.querySelector("#xpAt").value + "</Atual>\n<Max>" + document.querySelector("#xpMax").value + "</Max>\n</Xp>\n";
+
+//     let fim = "</Personagem>";
+
+//     return inicio + nome + raca + classe + tendencia + xp + fim;
+// }
 
 function pegarNome(){
     let nome = document.querySelector("#Nome").value;
